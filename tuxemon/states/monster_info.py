dbffc44113f8f17b657be047cabd377c8874e9db
Sanglorian <sanglorian@gmail.com>
2025-09-18 12:04:56 +1000

    Adding icon to show level of bond

--- tuxemon/states/monster_info.py
+++ tuxemon/states/monster_info.py
@@ -327,6 +327,24 @@ class MonsterInfoState(PygameMenuState):
             minus.translate(x + fxw(0.08), y)
 
 
+        # bond icon
+        bond_value = monster.bond
+
+        if bond_value <= 20:
+            bond_file = "gfx/ui/icons/bond/bond1.png"
+        elif bond_value <= 50:
+            bond_file = "gfx/ui/icons/bond/bond2.png"
+        elif bond_value <= 75:
+            bond_file = "gfx/ui/icons/bond/bond3.png"
+        else:
+            bond_file = "gfx/ui/icons/bond/bond4.png"
+
+        bond_icon = self._create_image(bond_file)
+        bond_icon.scale(prepare.SCALE, prepare.SCALE)
+        bond_widget = menu.add.image(image_path=bond_icon)
+        bond_widget.set_float(origin_position=True)
+
+        bond_widget.translate(fxw(13.4/256), fxh(26/144))
 
 
         # image

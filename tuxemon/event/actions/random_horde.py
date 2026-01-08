56cdd74a4f1a9d2d655d52b47ee32ba8bc85d1ef
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2026-01-08 14:57:44 +0100

    fix

--- tuxemon/event/actions/random_horde.py
+++ tuxemon/event/actions/random_horde.py
@@ -137,9 +137,7 @@ class RandomHordeAction(EventAction):
             battle_mode=BattleMode.SINGLE,
         )
         session.client.queue_state("CombatState", context=context)
-
-        session.client.movement_manager.lock_controls(player)
-        session.client.movement_manager.stop_char(player)
+        player.cancel_movement()
 
         rgb: ColorLike = WHITE_COLOR
         if self.rgb:

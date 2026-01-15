7f7eecdb24c986020bf4748cc47dd7f208bc615a
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2026-01-15 12:27:24 +0100

    scaling

--- tuxemon/ui/combat_layout.py
+++ tuxemon/ui/combat_layout.py
@@ -8,7 +8,6 @@ from typing import TYPE_CHECKING
 
 from pygame.rect import Rect
 
-from tuxemon.constants.paths import mods_folder
 from tuxemon.database.yaml_utils import load_yaml
 from tuxemon.scaling import DefaultScaling, ScalingStrategy
 
